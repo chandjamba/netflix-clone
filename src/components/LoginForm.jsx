4d59d67b1fd69181386928f3f4cab24d9d18f131
chandjamba <chandjamba2001@gmail.com>
2024-05-07 15:04:49 -0400

    netflix form page

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,41 @@
+export default function LoginForm() {
+  return (
+    <div className="wrapper">
+      <form action="">
+        <h1>Sign In</h1>
+        <div className="input-box">
+          <input type="text" placeholder="Email or mobile number" required />
+        </div>
+        <div className="input-box">
+          <input type="password" placeholder="Password" required />
+        </div>
+        <div className="sign-in-btn">
+          <button type="submit">Sign In</button>
+          <span className="or">OR</span>
+        </div>
+        <div className="sign-code-btn">
+          <button type="submit">Use a Sign-In Code</button>
+        </div>
+        <div className="forgot-password">
+          <a href="#">Forgot password?</a>
+        </div>
+        <div className="remember-forgot">
+          <input type="checkbox" /> Remember me
+        </div>
+        <div className="register-link">
+          <p>
+            {" "}
+            New to Netflix? <a href="#">Sign up now.</a>
+          </p>
+        </div>
+        <div className="reCaptcha-protection">
+          <p>
+            {" "}
+            This page is protected by Google reCAPTCHA to ensure you are not a
+            bot. <a href="#">Learn more.</a>
+          </p>
+        </div>
+      </form>
+    </div>
+  );
+}
