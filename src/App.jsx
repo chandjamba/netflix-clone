1320a087e81f998b7c2f625dee21158fc9780926
chandjamba <chandjamba2001@gmail.com>
2024-05-15 12:36:26 -0400

    work on main page api data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 import LoginForm from "./components/LoginForm";
+import StartPage from "./components/StartPage";
+import MainPage from "./components/MainPage";
 
 export default function App() {
-  return <div>
-    <LoginForm />
-  </div>;
+  return (
+    <BrowserRouter>
+      <header>
+        <nav>
+          <NavLink to="loginForm">Sign In</NavLink>
+          <NavLink to="mainPage">Main</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<StartPage />} />
+          <Route path="loginForm" element={<LoginForm />} />
+          <Route path="MainPage" element={<MainPage />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
 }
---
+++ src/components/StartPage.jsx
@@ -0,0 +1,27 @@
+export default function StartPage() {
+  return (
+    <div className="start-page-wrapper">
+      <h1>Unlimited movies, TV shows, and more</h1>
+      <div className="line-1">
+        <p>Watch anywhere. Cancel anytime.</p>
+      </div>
+      <div className="line-2">
+        <p>
+          Ready to watch? Enter your email to create or restart your membership.
+        </p>
+      </div>
+      <form action="">
+        <div className="sign-up-block">
+          <div className="sign-up-input">
+            <input type="text" placeholder="Email address" required />
+          </div>
+          <div className="get-start-btn">
+            <button type="submit">Get Started </button>
+          </div>
+        </div>
+          </form>
+      </div>
+      
+      
+  );
+}
