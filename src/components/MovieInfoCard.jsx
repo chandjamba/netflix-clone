74a69c2f6953dd7d869a3f9c8d5d83f2f0bb88c9
chandjamba <chandjamba2001@gmail.com>
2024-05-22 05:42:56 -0400

    Working on MovieInfoCard component

--- src/components/MovieInfoCard.jsx
+++ src/components/MovieInfoCard.jsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export default function MovieInfoCard() {
+  const [movieInfo, setMovieInfo] = useState();
+
   let { id } = useParams();
+  
+  
+  useEffect(()=> {
+    async function fetchedmovieinfo() {
+      const resp = await fetch(url, options);
+      const data = await resp.json();
+      console.log(data);
+      setMovieInfo(data.results);
+    }
+    fetchedmovieinfo();
+  }, []);
+
     return <div>
-      
+      {movieInfo}
   </div>;
-}
+  }
+
