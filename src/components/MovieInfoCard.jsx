fa13d1ad0923f47a77b8b1305405434f811ea69c
chandjamba <chandjamba2001@gmail.com>
2024-05-23 05:36:04 -0400

    Creating components for movie info card components

--- src/components/MovieInfoCard.jsx
+++ src/components/MovieInfoCard.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
+const img_base_url = "http://image.tmdb.org/t/p/";
+const img_size = "original";
+
 const options = {
   method: "GET",
   headers: {
@@ -27,5 +30,23 @@ export default function MovieInfoCard() {
     fetchedMovieInfo();
   }, [movieId]);
 
-  return <div>{movieInfo?.title}</div>;
+  return <div className="main-container">
+    <div className="heading-navbar">
+      <h1>{movieInfo?.title}</h1>
+      </div>
+    <div className="thumbnail-wrapper">
+      <div className="thumbnail">
+      <img
+          src={`${img_base_url}/${img_size}/${movieInfo?.poster_path}`}
+          alt=""
+        />
+      </div>
+    </div>
+    <div className="genres">
+      {/* <div>{movieInfo?.}</div> */}
+    </div>
+    <div className="overview">
+      <p>{movieInfo?.overview}</p>
+    </div>
+    </div>;
 }
